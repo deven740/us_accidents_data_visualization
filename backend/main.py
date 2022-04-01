80eac9d175d0ca3672593d8ec31a0731f3f3fb99
deven740 <devenparab740@gmail.com>
2022-04-01 23:49:59 +0530

    Importing users module to main file

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,14 @@
+from fastapi import FastAPI
+
 from typing import Optional
 
-from fastapi import FastAPI
+from users import users, models as user_models
+
+from database import engine
+
+
+user_models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World2"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+app.include_router(users.router)
