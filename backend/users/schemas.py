7a59e1a675c43b72aed42747955f596a0c19579a
deven740 <devenparab740@gmail.com>
2022-04-07 22:50:08 +0530

    Added validations to username and password

--- backend/users/schemas.py
+++ backend/users/schemas.py
@@ -1,6 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class UserSchema(BaseModel):
-    username: str
-    password: str
+    username: str = Field(min_length=8)
+    password: str = Field(min_length=8)
