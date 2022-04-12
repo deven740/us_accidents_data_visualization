def0941c0d6bacd321b8e56ed29cd706e293f301
deven740 <devenparab740@gmail.com>
2022-04-12 22:42:57 +0530

    Return access and refresh tokens

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -1,46 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 
 import "./Login.css";
 
 function Login() {
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(inputs);
+    // const data = {
+    //   username: values.username,
+    try {
+      const res = await axios.post(
+        `http://www.localhost:8000/users/login`,
+        inputs
+      );
+      console.log(res);
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  };
+
+  const handleChange = (e) => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div className="login">
       <div className="login-component">
-        <Formik
-          initialValues={{ username: "", password: "" }}
-          validate={(values) => {
-            const errors = {};
-            if (values.username.length < 8) {
-              errors.username = "Username cannot be less than 8 characters";
-            }
-            if (values.password.length < 8) {
-              errors.password = "Password cannot be less than 8 characters";
-            }
-            return errors;
-          }}
-          onSubmit={async (values) => {
-            console.log(values);
-            const data = {
-              username: "deven12345",
-              password: "deven12345",
-            };
-            await axios.post(`http://www.localhost:8000/users/login`, data);
-            // await axios.get("http://127.0.0.1:8000/users/").then((res) => {
-            //   console.log(res);
-            //   console.log(res.data);
-            // });
-          }}
-        >
-          <Form className="login-form">
-            <Field name="username" type="text" />
-            <ErrorMessage name="username" component="div" />
-            <Field name="password" type="password" />
-            <ErrorMessage name="password" component="div" />
-            <button type="submit">Submit</button>
-          </Form>
-        </Formik>
+        <h3>Login</h3>
+        <form action="post" className="login-form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="username"
+            value={inputs.username}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={inputs.password}
+            onChange={handleChange}
+          />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     </div>
   );
